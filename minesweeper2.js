3d08b2efce895313b7d92055a38b17a284b3fced
Sherly Chan <sherlyc@gmail.com>
2017-03-27 23:56:40 +1300

    changed some comment

--- minesweeper2.js
+++ minesweeper2.js
@@ -110,20 +110,9 @@ function checkForWin () {
       }
   }
 
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
 }
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`:
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through
-// them, counting the number of times `cell.isMine` is true.
-function countSurroundingMines (cell) {
+function countSurroundingMines (cell) { //count the number of mines around the cell
    var surrounding = lib.getSurroundingCells(cell.row, cell.col);
    var count = 0;
    for (var i=0; i< surrounding.length; i++){
